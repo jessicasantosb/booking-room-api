b3682279a8051565082365df04f4f01b2b613763
Jessica Bandeira <jessica.santosb@outlook.com>
2024-06-29 14:46:21 -0300

    feat: update rooms to know current bookings

--- routes/bookroomRoute.js
+++ routes/bookroomRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import BookroomModel from '../models/bookroom.js';
+import Rooms from '../models/rooms.js';
 
 const router = express.Router();
 
@@ -20,7 +21,12 @@ router.post('/bookroom', async (req, res) => {
       transactionid: uuidv4(),
     });
 
-    await newbooking.save();
+    const booking = await newbooking.save();
+    const roomtemp = await Rooms.findOne({ _id: roomid });
+    
+    roomtemp.currentbookings.push({bookingid: booking._id, fromDate: fromDate, toDate: toDate, userid: userid, status: booking.status})
+    
+    await roomtemp.save()
     res.send('Room booked successfully');
   } catch (error) {
     return res.status(400).json({ error });
