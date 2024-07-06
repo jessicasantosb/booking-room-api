2d22f7a530b8d1966df25515e51c466f83542b6f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-06 12:31:02 -0300

    feat: setup stripe to handle payments

--- routes/bookroomRoute.js
+++ routes/bookroomRoute.js
@@ -1,35 +1,71 @@
 import express from 'express';
+import Stripe from 'stripe';
 import { v4 as uuidv4 } from 'uuid';
 import BookroomModel from '../models/bookroom.js';
 import Rooms from '../models/rooms.js';
 
+const stripe = new Stripe(process.env.STRIPE_KEY);
 const router = express.Router();
 
 router.post('/bookroom', async (req, res) => {
-  const { room, roomid, userid, fromDate, toDate, totalAmount, totalDays } =
-    req.body;
+  const {
+    room,
+    roomid,
+    userid,
+    fromDate,
+    toDate,
+    totalAmount,
+    totalDays,
+    token,
+  } = req.body;
 
   try {
-    const newbooking = new BookroomModel({
-      room: room.name,
-      roomid: roomid,
-      userid: userid,
-      fromdate: fromDate,
-      todate: toDate,
-      totalamount: totalAmount,
-      totaldays: totalDays,
-      transactionid: uuidv4(),
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
     });
 
-    const booking = await newbooking.save();
-    const roomtemp = await Rooms.findOne({ _id: roomid });
-    
-    roomtemp.currentbookings.push({bookingid: booking._id, fromDate: fromDate, toDate: toDate, userid: userid, status: booking.status})
-    
-    await roomtemp.save()
-    res.send('Room booked successfully');
+    const payment = await stripe.charges.create(
+      {
+        amount: totalAmount * 100,
+        customer: customer.id,
+        currency: 'brl',
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
+
+    if (payment) {
+      const newbooking = new BookroomModel({
+        room: room.name,
+        roomid: roomid,
+        userid: userid,
+        fromdate: fromDate,
+        todate: toDate,
+        totalamount: totalAmount,
+        totaldays: totalDays,
+        transactionid: uuidv4(),
+      });
+
+      const booking = await newbooking.save();
+      const roomtemp = await Rooms.findOne({ _id: roomid });
+
+      roomtemp.currentbookings.push({
+        bookingid: booking._id,
+        fromDate: fromDate,
+        toDate: toDate,
+        userid: userid,
+        status: booking.status,
+      });
+
+      await roomtemp.save();
+    }
+
+    res.send('Payment has been made. Room booked successfully.');
   } catch (error) {
-    return res.status(400).json({ error });
+    console.error(error);
   }
 });
 
