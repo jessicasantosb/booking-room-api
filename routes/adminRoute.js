90e5d44c44b0982faae47f379ab41c16079d905a
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-08 19:37:42 -0300

    fix: import right model to get bookings

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import Rooms from '../models/rooms.js';
+import Bookings from '../models/bookroom.js';
 
 const router = express.Router();
 
 router.get('/getallbookings', async (req, res) => {
   try {
-    const bookings = await Rooms.find({});
+    const bookings = await Bookings.find({});
     res.send(bookings);
   } catch (error) {
     res.send(400).json({ error });
