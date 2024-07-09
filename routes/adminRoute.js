16823408abc8d13eb2e0343762bf6d6c6c48204b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-09 13:33:36 -0300

    feat: get all users

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import Bookings from '../models/bookroom.js';
 import Rooms from '../models/rooms.js';
+import Users from '../models/user.js';
 
 const router = express.Router();
 
@@ -15,10 +16,19 @@ router.get('/getallbookings', async (req, res) => {
 
 router.get('/getallrooms', async (req, res) => {
   try {
-    const rooms = await Rooms.find({})
-    res.send(rooms)
+    const rooms = await Rooms.find({});
+    res.send(rooms);
   } catch (error) {
-    res.send(400).json({message: error})
+    res.send(400).json({ message: error });
+  }
+});
+
+router.get('/getallusers', async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.send(users);
+  } catch (error) {
+    res.send(400).json({ message: error });
   }
 });
 
