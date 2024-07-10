d0bb30740996645c1a0a8fed44b376d8bf7b415b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-07-10 15:09:44 -0300

    feat: create a new room

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -10,7 +10,7 @@ router.get('/getallbookings', async (req, res) => {
     const bookings = await Bookings.find({});
     res.send(bookings);
   } catch (error) {
-    res.send(400).json({ message: error });
+    return res.status(400).json({ message: error });
   }
 });
 
@@ -19,7 +19,7 @@ router.get('/getallrooms', async (req, res) => {
     const rooms = await Rooms.find({});
     res.send(rooms);
   } catch (error) {
-    res.send(400).json({ message: error });
+    return res.status(400).json({ message: error });
   }
 });
 
@@ -28,8 +28,51 @@ router.get('/getallusers', async (req, res) => {
     const users = await Users.find({});
     res.send(users);
   } catch (error) {
-    res.send(400).json({ message: error });
+    return res.status(400).json({ message: error });
   }
 });
 
+router.post('/createroom', async (req, res) => {
+  const {
+    name,
+    rentPerDay,
+    maxCount,
+    phoneNumber,
+    type,
+    imageurl1,
+    imageurl2,
+    imageurl3,
+    description,
+  } = req.body;
+
+  try {
+    const newroom = new Rooms({
+      name: name,
+      rentproperty: rentPerDay,
+      maxcount: maxCount,
+      phonenumber: phoneNumber,
+      type: type,
+      imageurls: [imageurl1, imageurl2, imageurl3],
+      currentbookings: [],
+      description: description,
+    });
+
+    console.log(
+      name,
+      rentPerDay,
+      maxCount,
+      phoneNumber,
+      type,
+      imageurl1,
+      imageurl2,
+      imageurl3,
+      description
+    );
+
+    await newroom.save();
+    res.send('Room registered successfully');
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
 export default router;
