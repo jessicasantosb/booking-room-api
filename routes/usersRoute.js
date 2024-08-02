3de5ea0f78414720a87337504e754c445cadbf46
Jessica Bandeira <jessica.santosb@outlook.com>
2024-08-02 10:18:45 -0300

    feat: handle login with google

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,23 @@
 import bcrypt from 'bcrypt';
+import 'dotenv/config';
 import express from 'express';
+import { OAuth2Client } from 'google-auth-library';
 import User from '../models/user.js';
 
 const router = express.Router();
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const users = [];
+
+function upsert(array, item) {
+  const i = array.findIndex((_item) => _item.email === item.email);
+  if (i > -1) {
+    array[i] = item;
+  } else {
+    array.push(item);
+  }
+}
 
 router.post('/register', async (req, res) => {
   const notHashedPassword = req.body.passwordValue;
@@ -25,7 +40,7 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   const { emailValue, passwordValue } = req.body;
-  
+
   try {
     const user = await User.findOne({ email: emailValue });
     const passwordOk = user && bcrypt.compareSync(passwordValue, user.password);
@@ -38,6 +53,19 @@ router.post('/login', async (req, res) => {
   } catch (error) {
     return res.status(400).json({ error });
   }
+});
+
+router.post('/google-login', async (req, res) => {
+  const { token } = req.body;
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const { name, email, picture } = ticket.getPayload();
+  upsert(users, { name, email, picture });
+  res.status(201).json({ name, email, picture });
 });
 
 export default router;
